2b2743fa7b12ed1f58ca709f29fac6b4e8d54b4f
Vuong Do Tuan Thanh <79709336+thanhvdt@users.noreply.github.com>
2023-01-30 20:07:24 +0700

    Update rebel.py

--- Text-to-entities/rebel.py
+++ Text-to-entities/rebel.py
@@ -53,7 +53,7 @@ for c in clusters.values():
     print(c)
     
 # Open the file in write mode
-with open("output.txt", "w") as file:
+with open("cluster-output.txt", "w") as file:
     # Write multiple lines of text to the file
     for c in clusters.values():
         print(c, file = file)
@@ -134,6 +134,19 @@ class KB():
                     print(f"<R> {value}", end= " ")
                 elif (key == 'tail'):
                     print(f"<T> {value}", end= " ")
+
+        with open("relations-output.txt", "w") as file:
+            # Write multiple lines of text to the file
+            for r in self.relations:
+                    for key, value in r.items():
+                        if (key == 'head'):
+                            print(f"<H> {value}", end= " ", file = file)
+                        elif (key == 'type'):
+                            print(f"<R> {value}", end= " ", file = file)
+                        elif (key == 'tail'):
+                            print(f"<T> {value}", end= " ", file = file)
+
+
 
 def from_small_text_to_kb(text, verbose=False):
     kb = KB()
@@ -375,6 +388,17 @@ class KB():
                     print(f"<R> {value}", end= " ")
                 elif (key == 'tail'):
                     print(f"<T> {value}", end= " ")
+
+            with open("wiki-text-output.txt", "w") as file:
+                # Write multiple lines of text to the file
+                for r in self.relations:
+                        for key, value in r.items():
+                            if (key == 'head'):
+                                print(f"<H> {value}", end= " ", file = file)
+                            elif (key == 'type'):
+                                print(f"<R> {value}", end= " ", file = file)
+                            elif (key == 'tail'):
+                                print(f"<T> {value}", end= " ", file = file)
 
 # text = """
 # Napoleon Bonaparte (born Napoleone di Buonaparte; 15 August 1769 – 5 May 1821), and later known by his regnal name Napoleon I, was a French military and political leader who rose to prominence during the French Revolution and led several successful campaigns during the Revolutionary Wars. He was the de facto leader of the French Republic as First Consul from 1799 to 1804. As Napoleon I, he was Emperor of the French from 1804 until 1814 and again in 1815. Napoleon's political and cultural legacy has endured, and he has been one of the most celebrated and controversial leaders in world history. Napoleon was born on the island of Corsica not long after its annexation by the Kingdom of France.[5] He supported the French Revolution in 1789 while serving in the French army, and tried to spread its ideals to his native Corsica. He rose rapidly in the Army after he saved the governing French Directory by firing on royalist insurgents. In 1796, he began a military campaign against the Austrians and their Italian allies, scoring decisive victories and becoming a national hero. Two years later, he led a military expedition to Egypt that served as a springboard to political power. He engineered a coup in November 1799 and became First Consul of the Republic. Differences with the British meant that the French faced the War of the Third Coalition by 1805. Napoleon shattered this coalition with victories in the Ulm Campaign, and at the Battle of Austerlitz, which led to the dissolving of the Holy Roman Empire. In 1806, the Fourth Coalition took up arms against him because Prussia became worried about growing French influence on the continent. Napoleon knocked out Prussia at the battles of Jena and Auerstedt, marched the Grande Armée into Eastern Europe, annihilating the Russians in June 1807 at Friedland, and forcing the defeated nations of the Fourth Coalition to accept the Treaties of Tilsit. Two years later, the Austrians challenged the French again during the War of the Fifth Coalition, but Napoleon solidified his grip over Europe after triumphing at the Battle of Wagram. Hoping to extend the Continental System, his embargo against Britain, Napoleon invaded the Iberian Peninsula and declared his brother Joseph King of Spain in 1808. The Spanish and the Portuguese revolted in the Peninsular War, culminating in defeat for Napoleon's marshals. Napoleon launched an invasion of Russia in the summer of 1812. The resulting campaign witnessed the catastrophic retreat of Napoleon's Grande Armée. In 1813, Prussia and Austria joined Russian forces in a Sixth Coalition against France. A chaotic military campaign resulted in a large coalition army defeating Napoleon at the Battle of Leipzig in October 1813. The coalition invaded France and captured Paris, forcing Napoleon to abdicate in April 1814. He was exiled to the island of Elba, between Corsica and Italy. In France, the Bourbons were restored to power. However, Napoleon escaped Elba in February 1815 and took control of France.[6][7] The Allies responded by forming a Seventh Coalition, which defeated Napoleon at the Battle of Waterloo in June 1815. The British exiled him to the remote island of Saint Helena in the Atlantic, where he died in 1821 at the age of 51. Napoleon had an extensive impact on the modern world, bringing liberal reforms to the many countries he conquered, especially the Low Countries, Switzerland, and parts of modern Italy and Germany. He implemented liberal policies in France and Western Europe.
